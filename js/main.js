6062c8f4cf2592cc0ef571ab0b4010352bcf5b7d
esimabol <edylber.simancas@outlook.com>
2021-09-10 15:44:22 -0500

    Agregando Efecto al Titulo del Hero

--- js/main.js
+++ js/main.js
@@ -1,35 +1,47 @@
 let contentBtn = document.querySelector('#displayContent');	
-	contentBtn.addEventListener('click', displayContent, false);
+contentBtn.addEventListener('click', displayContent, false);
 
 let iconBtn = document.querySelector('.icon');
-	iconBtn.addEventListener('click', showMenu, false);
+iconBtn.addEventListener('click', showMenu, false);
+
+let i =0;
+let text = 'Hi, I am Eddy Simancas';
 
 
-	function displayContent(){			
-	  let x = document.getElementById("profile");
-	  if (x.innerHTML === "As a developer...") {
-	    x.innerHTML = "As a developer, designing web applications for both front-end and back-end, creating user interfaces and forms using HTML5, CSS3, Bootstrap and, making conenctions to the back-end using JavaScript and J-query same as managing SQL databases running basic, complex queries and performing calculations using the SQL language using MySQL software.";
-	  } else {
-	    x.innerHTML ='As a developer...';
-	    
-	  }
-	  if (contentBtn.value === "Learn More") {
-	  		contentBtn.value = "Hide";
-	  }
-	  else{
-	  		contentBtn.value = "Learn More";
-	  }
+function displayContent(){			
+  let x = document.getElementById("profile");
+  if (x.innerHTML === "As a developer...") {
+    x.innerHTML = "As a developer, designing web applications for both front-end and back-end, creating user interfaces and forms using HTML5, CSS3, Bootstrap and, making conenctions to the back-end using JavaScript and J-query same as managing SQL databases running basic, complex queries and performing calculations using the SQL language using MySQL software.";
+  } else {
+    x.innerHTML ='As a developer...';
+    
+  }
+  if (contentBtn.value === "Learn More") {
+  		contentBtn.value = "Hide";
+  }
+  else{
+  		contentBtn.value = "Learn More";
+  }
+}
+
+function showMenu() {
+  var x = document.querySelector(".nav");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else if(x.style.display === "flex") {
+    x.style.display = "none";
+  }
+  else{
+  	x.style.display = "flex";
+  }
+}
+
+function textEffect() {
+	if (i < text.length) {
+		document.querySelector('#typewriter').innerHTML += text.charAt(i);
+		i++;
+		setTimeout(textEffect, 300);
 	}
+}
 
-	function showMenu() {
-	  var x = document.querySelector(".nav");
-	  if (x.style.display === "none") {
-	    x.style.display = "flex";
-	  } else if(x.style.display === "flex") {
-	    x.style.display = "none";
-	  }
-	  else{
-	  	x.style.display = "flex";
-	  }
-	}
-
+textEffect();
